13f2413476988ea7b688e8d8c38968ce47a1cb14
Nadeem khan <khannadeem1892@gmail.com>
2024-09-10 14:08:14 +0530

    Login EndPoint RTK Query

--- src/Service/FatchApi.js
+++ src/Service/FatchApi.js
@@ -1,1 +1,21 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+
+ export const authApi = createApi({
+    reducerPath:"authApi",
+    baseQuery: fetchBaseQuery({baseUrl: "http://localhost:3000/"}),
+    tagTypes: ["users"],
+    endpoints: (builder) => ({
+    // login End point
+    login:builder.mutation({
+        query: () => ({
+
+    })
+    }),
+      // login End point
+    //   register:builder.mutation({
+    //     query: () => ({
+
+    //  })
+    // }),
+    })
+ })
